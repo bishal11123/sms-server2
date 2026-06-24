ac3fbb9143c63271132320e25c6a98f26f33a09e
bishal11123 <68977623+bishal11123@users.noreply.github.com>
2026-06-24 15:18:26 +0545

    Update Auth0 issuerBaseURL in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,5 +4,5 @@ import { auth } from "express-oauth2-jwt-bearer";
 // ✅ Protect routes using Auth0 JWT verification
 export const authenticateToken = auth({
   audience: "https://myapi.example.com", // 👈 your Auth0 API Identifier
-  issuerBaseURL: "https://dev-y3b7afevz32ju2k1.us.auth0.com/",
+  issuerBaseURL: "https://dev-ad1yk6aem714tjpu.us.auth0.com/",
 });
